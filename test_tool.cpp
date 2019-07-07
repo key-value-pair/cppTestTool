6aa72f04798b685d04fc66d2734c4b8e21d365e9
key-value-pair <key-value-pair@outlook.com>
2019-07-07 16:24:58 +0800

    show number of all, fail and pass

--- test_tool.cpp
+++ test_tool.cpp
@@ -4,15 +4,27 @@ std::vector<std::function<void(void)>> functionList;
 
 void runTest() {
   std::vector<AssertFail> afList;
+  std::vector<bool> passList;
   
   for (auto fun : functionList) {
     try {
       fun();
+      passList.push_back(true);
     } catch (const AssertFail& af) {
       afList.push_back(af);
+      passList.push_back(false);
     }
   }
 
+  std::cerr << "all: " << functionList.size()
+	    << ", fail: " << afList.size()
+	    << ", pass: " << (functionList.size()-afList.size())
+	    << std::endl;
+
+  for (auto p : passList)
+    std::cerr << (p ? "." : "x");
+  std::cerr << std::endl;
+  
   for (auto af : afList)
     std::cerr << af.what() << std::endl;
 }
